11f0885fa9cbf253025304112ef9ae7aae05a075
Adrien Hors <adrienhors1@gmail.com>
2022-08-17 13:02:31 +0200

    Rails Yelp MVp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+oishi = { name: 'Oishi', address: '7 rue de Rennes', category: 'japanese' }
+pizza = { name: 'Pizza', address: '53 Place Monge', category: 'italian' }
+joe_burger = { name: 'Joe Burger', address: '4 rue Mouffetard', category: 'french' }
+pot_o_lait = { name: 'Pot O Lait', address: 'Place Contrescarpe', category: 'belgian' }
+chez_ann = { name: 'Chez Ann', address: '18 rue Moufetard', category: 'chinese' }
+
+[oishi, pizza, joe_burger, pot_o_lait, chez_ann].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
